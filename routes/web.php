8ff6d47db1e192dcd465fb9d43f17b13035d865f
unknown <aawwaaww387@gmail.com>
2024-12-12 18:59:07 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    $name = 'Ahmed';
+    $departments =[
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    
+    //return view('about')->with(key: 'name', value: $name);
+    return view('about', compact('name', 'departments'));
+
+});
+Route::post('/about', function(){
+    $name = $_POST['name'];
+    $departments =[
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+
+    return view('about', compact(var_name: 'name'));
+});
